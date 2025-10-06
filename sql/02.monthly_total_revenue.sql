51ccc31a3ebefb2623e0667dcf118386f4a5902b
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-06 17:27:01 +0700

    02.monthly_total_revenue.sql

--- sql/02.monthly_total_revenue.sql
+++ sql/02.monthly_total_revenue.sql
@@ -4,6 +4,6 @@ SELECT
   EXTRACT(month FROM transaction_date) AS month, -- Memilih bulan dari kolom `transaction_date`
   SUM(total_paid) AS total_revenue  -- Menjumlahkan nilai pada kolom `total_paid`
 FROM `FinalProject.transaction_detail` -- Mengambil data dari tabel `transaction_detail`
-WHERE EXTRACT(year FROM transaction_date) = 2024   -- Memfilter tahun transaksi hanya 2024
-GROUP BY month  -- Menampilkan total_revenue berdasarkan bulan
+WHERE EXTRACT(year FROM transaction_date) = 2024  -- Memfilter tahun transaksi dari kolom `transaction_date` hanya 2024
+GROUP BY month  -- Mengelompokkan total_revenue berdasarkan bulan
 ORDER BY month ASC;  -- Mengurutkan hasil mulai dari bulan terkecil 
