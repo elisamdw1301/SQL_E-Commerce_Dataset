05b0fecd4dc1882ca8ebed5ab932a04eda7703b6
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-07 10:36:54 +0700

    04_monthly_channel_growth.sql

--- sql/04_monthly_channel_growth.sql
+++ sql/04_monthly_channel_growth.sql
@@ -1,7 +1,6 @@
 -- Pertumbuhan channel bulanan.
--- 1. Total orders (distinct) dan revenue (after_discount) per bulan.
--- 2. Hitung MoM growth revenue per bulan vs 2023 dalam bulan yang sama
 
+-- 1. Total orders (distinct) dan revenue (after_discount) per bulan tahun 2023 dan 2024
 WITH 
   orders_2024 AS (  -- CTE pertama untuk menampilkan data tahun 2024
     SELECT
@@ -30,7 +29,7 @@ WITH
     FROM orders_2024
     GROUP BY month, channel_type
   ),
-  agg_2023 AS (  -- CTE keempat untuk menampilkan total order dan total pendapatan tahun 2023
+  agg_2023 AS (  -- CTE keempat untuk menampilkan total pendapatan tahun 2023
     SELECT
       month,
       channel_type,
@@ -38,22 +37,26 @@ WITH
     FROM orders_2023
     GROUP BY month, channel_type
   )
+
+-- 2. Hitung YoY growth revenue per bulan 2024 vs 2023 dalam bulan yang sama
 SELECT 
-  FORMAT_DATE('%B', DATE(2024, a24.month, 1)) AS period,
+  FORMAT_DATE('%B', DATE(2024, a24.month, 1)) AS period,  -- Menampilkan nama bulan
   a24.channel_type AS channel,
   a24.total_orders,
-  ROUND(a24.revenue_2024, 2) AS revenue_2024,
-  ROUND(COALESCE(a23.revenue_2023, 0), 2) AS revenue_2023,
-  CONCAT(ROUND(SAFE_DIVIDE(a24.revenue_2024 - COALESCE(a23.revenue_2023, 0), NULLIF(a23.revenue_2023, 0)) * 100, 2), '%') AS yoy_growth,
+  ROUND(a24.revenue_2024, 2) AS revenue_2024,  -- Menampilkan total pendapatan tahun 2024 dengan format 2 angka dibelakang koma
+  ROUND(COALESCE(a23.revenue_2023, 0), 2) AS revenue_2023,  -- Menampilkan total pendapatan tahun 2023 dengan format 2 angka dibelakang koma
+  -- Menghitung Year-over-Year dengan rumus ((pendapatan 2024-pendapatan 2023) / pendapatan 2023) * 100 untuk mendapatkan hasil dalam persentase
+  CONCAT(ROUND(SAFE_DIVIDE(a24.revenue_2024 - COALESCE(a23.revenue_2023, 0), NULLIF(a23.revenue_2023, 0)) * 100, 2), '%') AS yoy_growth,  
+  -- Menampilkan status jika memenuhi salah satu dari 4 kondisi berdasarkan persentase YoY
   CASE
     WHEN a23.revenue_2023 IS NULL OR a23.revenue_2023 = 0 THEN 'New Channel'
     WHEN SAFE_DIVIDE(a24.revenue_2024 - a23.revenue_2023, a23.revenue_2023) > 0.1 THEN 'Strong Growth'
     WHEN SAFE_DIVIDE(a24.revenue_2024 - a23.revenue_2023, a23.revenue_2023) > 0 THEN 'Growth'
-    ELSE 'Decline'
+    ELSE 'Decline' 
   END AS status
 FROM agg_2024 AS a24
-JOIN agg_2023 AS a23
+LEFT JOIN agg_2023 AS a23  -- Menggabungkan 2 CTE dengan mempertahankan semua data di CTE `agg_2024`
 ON 
-  a24.month = a23.month 
-  AND a24.channel_type = a23.channel_type
+  a24.month = a23.month  -- Mencocokkan data berdasarkan bulan yang sama 
+  AND a24.channel_type = a23.channel_type  -- Mencocokkan data berdasarkan jenis saluran yang sama
 ORDER BY a24.month, a24.revenue_2024 DESC;
