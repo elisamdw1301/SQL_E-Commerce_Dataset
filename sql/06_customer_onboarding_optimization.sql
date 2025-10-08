a74f4c572736f7187c04dfe315b471af0d795660
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:53:17 +0700

    06_customer_onboarding_optimization.sql

--- sql/06_customer_onboarding_optimization.sql
+++ sql/06_customer_onboarding_optimization.sql
@@ -1,23 +1,36 @@
--- Mengetahui optimasi customer onboarding.
+-- ANALISIS 5
 
-WITH first_order AS (  -- Membuat CTE untuk menampilkan data pelanggan baru
+-- Mengetahui optimasi customer onboarding
+
+-- 1. Membuat CTE untuk menampilkan data pelanggan baru
+WITH first_order AS (  
     SELECT
       customer_id,
-      MIN(order_date) AS first_order_date  -- Menampilkan data tanggal pesanan yang paling awal
-    FROM `FinalProject.order_detail`
-    GROUP BY customer_id  -- Mengelompokkan hasil berdasarkan `customer_id`
+      MIN(order_date) AS first_order_date  
+    FROM 
+        `FinalProject.order_detail`
+    GROUP BY 
+        customer_id 
 )
+    
+-- 2. Menghitung rata-rata waktu
 SELECT
-    EXTRACT(MONTH FROM cd.registration_date) AS month,  -- Memilih bulan dari tanggal registrasi
+    EXTRACT(MONTH FROM cd.registration_date) AS month,  
     cd.registration_channel,
-    COUNT(DISTINCT cd.customer_id) AS customer_new,  -- Menghitung banyaknya id customer
-    -- Menghitung rata-rata waktu dari selisih hari pada tanggal pesanan yang paling awal dan tanggal registrasi
+    COUNT(DISTINCT cd.customer_id) AS customer_new,  
+-- Menghitung rata-rata waktu dari selisih hari pada tanggal pesanan yang paling awal dan tanggal registrasi
     AVG(DATE_DIFF(DATE(fo.first_order_date), DATE(cd.registration_date), DAY)) AS avg_registration_to_first_order
-FROM `FinalProject.customer_detail_cleaned` AS cd
-JOIN first_order AS fo  -- Menggabungkan CTE dengan tabel `customer_detail_cleaned`
-ON cd.customer_id = fo.customer_id  -- Mencocokkan data berdasarkan `customer_id`
+FROM 
+    `FinalProject.customer_detail_cleaned` AS cd
+-- Menggabungkan CTE dengan tabel `customer_detail_cleaned` berdasarkan `customer_id` yang sama
+JOIN 
+    first_order AS fo  
+ON 
+    cd.customer_id = fo.customer_id  
 WHERE 
-  EXTRACT(YEAR FROM cd.registration_date) = 2024  -- Memfilter tanggal registrasi hanya tahun 2024
-  AND fo.first_order_date IS NOT NULL  -- Memfilter hanya tanggal pesanan paling awal yang tidak NULL
-GROUP BY month, cd.registration_channel  -- Mengelompokkan hasil berdasarkan bulan dan channel registrasi
-ORDER BY month, cd.registration_channel;  -- Mengurutkan hasil dengan bulan dan channel registrasi yang terkecil
+  EXTRACT(YEAR FROM cd.registration_date) = 2024  
+  AND fo.first_order_date IS NOT NULL  
+GROUP BY 
+    month, cd.registration_channel 
+ORDER BY 
+    month, cd.registration_channel ASC;  
