8ed068675d9f7b7c14a65347edfcb1d9d5312ef6
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:30:10 +0700

    03_sales_trend_by_category.sql

--- sql/03_sales_trend_by_category.sql
+++ sql/03_sales_trend_by_category.sql
@@ -1,16 +1,19 @@
--- Tren Penjualan Kategori 2020–2024.
+-- ANALISIS 2
 
+
+-- Menampilkan tren penjualan berdasarkan kategori
 SELECT    
-    p.category, -- Menampilkan kolom `category` dari tabel `product_detail`
-    -- Menjumlahkan nilai pada kolom `quantity` berdasarkan tahun pada kolom `order_date`
+    p.category, 
+-- Menjumlahkan nilai pada kolom `quantity` berdasarkan tahun pada kolom `order_date`
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2020 THEN o.quantity ELSE 0 END) AS year_2020, 
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2021 THEN o.quantity ELSE 0 END) AS year_2021,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2022 THEN o.quantity ELSE 0 END) AS year_2022,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2023 THEN o.quantity ELSE 0 END) AS year_2023,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2024 THEN o.quantity ELSE 0 END) AS year_2024
 FROM `FinalProject.order_detail` AS o    
-JOIN `FinalProject.product_detail` AS p    -- Menggabungkan tabel `order_detail` dan `product_detail` 
-ON o.sku_id = p.sku_id    -- Mencocokkan tabel berdasarkan `sku_id` yang sama     
+-- Menggabungkan tabel `order_detail` dan `product_detail` berdasarkan kolom `sku_id` yang sama
+JOIN `FinalProject.product_detail` AS p    
+ON o.sku_id = p.sku_id      
 WHERE EXTRACT(YEAR FROM o.order_date) BETWEEN 2020 AND 2024    
 GROUP BY p.category
 ORDER BY p.category;
