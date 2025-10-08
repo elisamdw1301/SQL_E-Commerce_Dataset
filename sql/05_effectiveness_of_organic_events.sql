a94eaffa04987ac132b31e7e6cf5d77bf312dc58
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:48:22 +0700

    05_effectiveness_of_organic_events.sql

--- sql/05_effectiveness_of_organic_events.sql
+++ sql/05_effectiveness_of_organic_events.sql
@@ -1,20 +1,29 @@
--- Efektifitas jalur organic
+-- ANALISIS 4
 
-WITH organic AS (  -- CTE untuk menampilkan data dari tabel `funnel_detail`
+-- Menganalisis efektifitas jalur organic
+
+-- CTE untuk menampilkan data dari tabel `funnel_detail`
+WITH organic AS (  
   SELECT
     channel_source,
     order_id
-  FROM `FinalProject.funnel_detail`
+  FROM 
+    `FinalProject.funnel_detail`
   WHERE 
-    event = 'Organic'  -- Memfilter kolom `event` hanya menampilkan data `Organic`
-    AND DATE(funnel_date) BETWEEN '2024-01-01' AND '2024-12-31'  -- Memfilter kolom `funnel_date` untuk menampilkan data selama tahun 2024
+    event = 'Organic'  
+    AND DATE(funnel_date) BETWEEN '2024-01-01' AND '2024-12-31' 
 )
 SELECT  
   channel_source,
-  COUNT(*) AS total_events,  -- Menampilkan banyaknya data(baris) dalam CTE yang telah dibuat
-  COUNT(DISTINCT NULLIF(order_id, '')) AS total_converted_orders,  -- Menampilkan banyaknya data berdasarkan `order_id`
-  -- Menampilkan persentase dengan rumus ((banyaknya data `order_id`/ banyaknya data(baris) dalam CTE) * 100)
+-- Menampilkan banyaknya data(baris) dalam CTE yang telah dibuat
+  COUNT(*) AS total_events,  
+-- Menampilkan banyaknya data berdasarkan `order_id`
+  COUNT(DISTINCT NULLIF(order_id, '')) AS total_converted_orders,  
+-- Menampilkan persentase dengan rumus ((banyaknya data `order_id`/ banyaknya data(baris) dalam CTE) * 100)
   CONCAT((SAFE_DIVIDE(COUNT(DISTINCT NULLIF(order_id, '')), COUNT(*)) * 100), '%') AS conversion_rate
-FROM organic -- Mengambil data dari CTE yang telah dibuat
-GROUP BY channel_source  -- Mengelompokkan hasil berdasarkan `channel_source`
-ORDER BY total_events DESC; -- Mengurutkan hasil berdasarkan `total_events` dari yang terbesar
+FROM 
+  organic 
+GROUP BY 
+  channel_source  
+ORDER BY 
+  total_events DESC; 
