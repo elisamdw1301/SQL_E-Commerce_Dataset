9e5ac1b0caa1c8e863b01e3e604d43735e961012
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:25:45 +0700

    02_monthly_total_revenue.sql

--- sql/02_monthly_total_revenue.sql
+++ sql/02_monthly_total_revenue.sql
@@ -1,9 +1,11 @@
--- Total pendapatan per bulan tahun 2024.
+-- Analisis 1
 
+
+-- Menghitung total pendapatan per bulan tahun 2024
 SELECT
-  EXTRACT(month FROM transaction_date) AS month, -- Memilih bulan dari kolom `transaction_date`
-  SUM(total_paid) AS total_revenue  -- Menjumlahkan nilai pada kolom `total_paid`
-FROM `FinalProject.transaction_detail` -- Mengambil data dari tabel `transaction_detail`
-WHERE EXTRACT(year FROM transaction_date) = 2024  -- Memfilter tahun transaksi dari kolom `transaction_date` hanya 2024
-GROUP BY month  -- Mengelompokkan total_revenue berdasarkan bulan
-ORDER BY month ASC;  -- Mengurutkan hasil mulai dari bulan terkecil 
+  EXTRACT(MONTH FROM transaction_date) AS month, 
+  SUM(total_paid) AS total_revenue  
+FROM `FinalProject.transaction_detail` 
+WHERE EXTRACT(YEAR FROM transaction_date) = 2024  
+GROUP BY month  
+ORDER BY month ASC;  
