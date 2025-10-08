c77932ddbc8e708fff450f9f76e7f13b3c94a449
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:54:54 +0700

    01_data_cleaning.sql

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -10,17 +10,19 @@ CREATE OR REPLACE TABLE `FinalProject.customer_detail_cleaned` AS  -- Membuat ta
     TRIM(`customer_type `) AS customer_type,  -- Menghapus spasi berlebih pada kolom `customer_type`
     TRIM(`province `) AS province,  -- Menghapus spasi berlebih pada kolom `province`
     TRIM(`registration_channel `) AS registration_channel  -- Menghapus spasi berlebih pada kolom `registration_channel`
-  FROM `FinalProject.customer_detail`;  -- Mengambil data dari tabel `customer_detail`
+  FROM 
+    `FinalProject.customer_detail`;  -- Mengambil data dari tabel `customer_detail`
 
 -- 2. Menangani nilai NULL pada kolom provinsi
 SELECT  -- Mengambil data
   customer_id,  -- Memilih kolom `customer_id`
   COALESCE(province, 'UNKNOWN') AS province_cleaned   -- Mengganti nilai NULL kolom `province` dengan `UNKNOWN`
-FROM `FinalProject.customer_detail_cleaned`;  -- Mengambil data dari tabel `customer_detail_cleaned`
+FROM 
+  `FinalProject.customer_detail_cleaned`;  -- Mengambil data dari tabel `customer_detail_cleaned`
 
 -- 3. Menangani nilai NULL pada kolom tanggal
 SELECT  -- Mengambil data
   *,  -- Memilih seluruh data
-  -- Mengubah nilai NULL pada kolom `order_date` dengan tanggal baru 
-  COALESCE(order_date, CAST('2000-01-01 00:00:00 UTC' AS TIMESTAMP)) AS order_date_cleaned
-FROM `FinalProject.order_detail`  -- Mengambil data dari tabel `order_detail`
+  COALESCE(order_date, CAST('2000-01-01 00:00:00 UTC' AS TIMESTAMP)) AS order_date_cleaned -- Mengubah nilai NULL pada kolom `order_date` dengan tanggal baru 
+FROM 
+  `FinalProject.order_detail`  -- Mengambil data dari tabel `order_detail`
