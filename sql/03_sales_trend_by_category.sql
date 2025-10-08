a9fdd0e1d2efd66e40b7503f9388ba7434da8a42
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-08 10:56:34 +0700

    03_sales_trend_by_category.sql

--- sql/03_sales_trend_by_category.sql
+++ sql/03_sales_trend_by_category.sql
@@ -1,6 +1,7 @@
 -- ANALISIS 2
 
 -- Menampilkan tren penjualan berdasarkan kategori
+
 SELECT    
     p.category, 
 -- Menjumlahkan nilai pada kolom `quantity` berdasarkan tahun pada kolom `order_date`
@@ -9,10 +10,16 @@ SELECT
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2022 THEN o.quantity ELSE 0 END) AS year_2022,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2023 THEN o.quantity ELSE 0 END) AS year_2023,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2024 THEN o.quantity ELSE 0 END) AS year_2024
-FROM `FinalProject.order_detail` AS o    
+FROM 
+    `FinalProject.order_detail` AS o    
 -- Menggabungkan tabel `order_detail` dan `product_detail` berdasarkan kolom `sku_id` yang sama
-JOIN `FinalProject.product_detail` AS p    
-ON o.sku_id = p.sku_id      
-WHERE EXTRACT(YEAR FROM o.order_date) BETWEEN 2020 AND 2024    
-GROUP BY p.category
-ORDER BY p.category;
+JOIN 
+    `FinalProject.product_detail` AS p    
+ON 
+    o.sku_id = p.sku_id      
+WHERE 
+    EXTRACT(YEAR FROM o.order_date) BETWEEN 2020 AND 2024    
+GROUP BY 
+    p.category
+ORDER BY 
+    p.category;
