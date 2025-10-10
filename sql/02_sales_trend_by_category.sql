0f7109f536ac20b302684b240cf49e88b410502f
Elisa Martiana Dewi <elisa.martiana.dewi1301@gmail.com>
2025-10-10 16:49:07 +0700

    02_sales_trend_by_category.sql

--- sql/02_sales_trend_by_category.sql
+++ sql/02_sales_trend_by_category.sql
@@ -4,7 +4,7 @@
 
 SELECT    
     p.category, 
--- Sum the values in the `quantity` column based on the year in the `order_date` column
+-- Calculating the amount of the values in the `quantity` column based on the year in the `order_date` column
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2020 THEN o.quantity ELSE 0 END) AS year_2020, 
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2021 THEN o.quantity ELSE 0 END) AS year_2021,
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2022 THEN o.quantity ELSE 0 END) AS year_2022,
@@ -12,7 +12,7 @@ SELECT
     SUM(CASE WHEN EXTRACT(YEAR FROM o.order_date) = 2024 THEN o.quantity ELSE 0 END) AS year_2024
 FROM 
     `FinalProject.order_detail` AS o    
--- Join the `order_detail` and `product_detail` tables based on the same `sku_id` column
+-- Combining the `order_detail` and `product_detail` tables based on the same `sku_id` column
 JOIN 
     `FinalProject.product_detail` AS p    
 ON 
